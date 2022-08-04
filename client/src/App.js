ab560afb270dbab4dd595a936d855e0f58d7ed9b
nimishsantosh107 <36289353+nimishsantosh107@users.noreply.github.com>
2022-08-04 05:38:02 +0530

    navbar section done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-import "./App.css";
-
 function App() {
-    return <div className="App">App</div>;
+    return (
+        <div className="min-h-screen">
+            <div className="gradient-bg-welcome">
+                <Navbar />
+                <Welcome />
+            </div>
+            <Services />
+            <Transactions />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
