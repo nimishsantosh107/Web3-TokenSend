9a0dd293705ee7169c696871c540040c16a07b00
nimishsantosh107 <36289353+nimishsantosh107@users.noreply.github.com>
2022-08-05 06:22:13 +0530

    added footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,23 @@
 const Footer = (props) => {
-    return <div>Footer</div>;
+    return (
+        <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+            <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+                <div className="flex flex-[0.5] justify-center items-center">
+                    <h1 className="text-3xl text-white font-extrabold">SoapSend</h1>
+                </div>
+                <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
+                    <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
+                    <p className="text-white text-base text-center mx-2 cursor-pointer">Exchange</p>
+                    <p className="text-white text-base text-center mx-2 cursor-pointer">
+                        Tutorials
+                    </p>
+                    <p className="text-white text-base text-center mx-2 cursor-pointer">
+                        Contact Us
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
